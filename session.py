9dbf10db8f320847c995205501660dacd9fb3bd6
Fabian Knorr <ers.trion@gmail.com>
2016-05-20 20:25:31 +0200

    Partially adapted session.py to new DB

--- session.py
+++ session.py
@@ -47,15 +47,13 @@ class Session:
             course["sync"] = { "y" : "yes", "n" : "no", "m" : "metadata only" }[sync]
             self.db.add_course(course_id, course)
 
-        sync_courses = self.db.list_courses(select_sync_no=False)
+        sync_courses = self.db.list_courses(full=True, select_sync_no=False)
         last_course_synced = False
         db_files = self.db.list_files()
-        for course_id in sync_courses:
-            course = self.db.get_course_details(course_id)
-
+        for course in sync_courses:
             base_url = self.config["studip_base"] + "/studip"
-            course_url = base_url + "/seminar_main.php?auswahl=" + course_id
-            folder_url = base_url + "/folder.php?cid=" + course_id + "&cmd=all"
+            course_url = base_url + "/seminar_main.php?auswahl=" + course.id
+            folder_url = base_url + "/folder.php?cid=" + course.id + "&cmd=all"
 
             try:
                 self.http.get(course_url, timeout=(None, 0))
@@ -70,7 +68,7 @@ class Session:
             if last_course_synced:
                 print()
 
-            new_files = [file_id for file_id in file_list if file_id not in db_files]
+            new_files = [ file_id for file_id in file_list if file_id not in db_files ]
             if len(new_files) > 0 :
                 if not last_course_synced:
                     print()
@@ -79,7 +77,7 @@ class Session:
             else:
                 print("No", end="")
                 last_course_synced = False
-            print(" new files for " + course["name"])
+            print(" new files for " + course.name)
 
             for i, file_id in enumerate(new_files):
                 print("Fetching metadata for file {}/{}...".format(i+1, len(new_files)),
@@ -89,8 +87,8 @@ class Session:
                 r = self.http.get(open_url)
                 details = parse_file_details(r.text)
                 if all(attr in details for attr in ["name", "url", "folder"]):
-                    details["course"] = course_id
-                    self.db.add_file(file_id, details)
+                    file = database.File(file_id, details["name"], date.today())
+                    self.db.add_file(file)
                     print(" " + details["description"])
                 else:
                     print(" <bad format>")
@@ -98,7 +96,7 @@ class Session:
 
     def download_files(self):
         first_file = True
-        for file in self.db.list_file_details(sync_courses_only=True):
+        for file in self.db.list_files(full=True, sync_courses_only=True):
             dir_path = self.sync_dir + "/" + file["path"]
             os.makedirs(dir_path, exist_ok=True)
             rel_path = file["path"] + "/" + file["name"]
